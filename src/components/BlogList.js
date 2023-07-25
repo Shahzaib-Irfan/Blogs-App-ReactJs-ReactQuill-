f8bbe2c0e1ac566902b37588d3d1f69890662fc2
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-07-25 16:52:30 +0500

    Second Commit

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -7,15 +7,19 @@ const BlogList = ({ blogs }) => {
       <center>
         <h2>Blogs</h2>
       </center>
-      {blogs.map((blog, index) => {
-        return (
-          <div key={index}>
-            <Link style={{ textTransform: "none" }} to={`/blog/${index}`}>
-              <h3>{blog.title}</h3>
-            </Link>
-          </div>
-        );
-      })}
+      blogs ?{" "}
+      <>
+        {blogs.map((blog, index) => {
+          return (
+            <div key={index}>
+              <Link style={{ textTransform: "none" }} to={`/blog/${index}`}>
+                <h3>{blog.title}</h3>
+              </Link>
+            </div>
+          );
+        })}
+      </>{" "}
+      : <center>No Blogs Found</center>
     </div>
   );
 };
